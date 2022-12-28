630b85502c6499f1c05ed3ab882accc0f9dd0e22
mthanh2209 <mongthanh.220902@gmail.com>
2022-12-28 21:05:15 +0700

    chuc nang dang nhap

---
+++ register.php
@@ -0,0 +1,82 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $user_type = $_POST['user_type'];
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+      $message[] = 'Tài khoản đã tồn tại';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$cpass', '$user_type')") or die('query failed');
+         $message[] = 'registered successfully!';
+         header('location:login.php');
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>đăng ký</h3>
+      <input type="text" name="name" placeholder="nhập tên" required class="box">
+      <input type="email" name="email" placeholder="nhập email" required class="box">
+      <input type="password" name="password" placeholder="nhập mật khẩu" required class="box">
+      <input type="password" name="cpassword" placeholder="nhập lại mật khẩu" required class="box">
+      <select name="user_type" class="box">
+         <option value="user">người dùng</option>
+         <option value="admin">quản trị viên</option>
+      </select>
+      <input type="submit" name="submit" value="Đăng Ký" class="btn">
+      <p>Bạn đã có tài khoản? <a href="login.php"> Đăng nhập</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
